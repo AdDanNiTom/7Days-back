4328e6e4441ef470217a83fd3248f3d5c73da029
AdrianAranda <adrienaranda@gmail.com>
2021-12-08 16:40:31 +0100

    corrected Model

--- models/Users.model.js
+++ models/Users.model.js
@@ -2,18 +2,19 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const usersSchema = new Schema({
-    username: {
-        type: String,
-        unique: true // `username` must be unique
-      },
-    firstName: String,
-    email: {
-        type: String,
-        unique: true // `email` must be unique
-      },
-    lastName: String,
-    description: String,
-    profilePhoto: String,
-})
+  username: {
+    type: String,
+    unique: true, // `username` must be unique
+  },
+  firstName: String,
+  email: {
+    type: String,
+    unique: true, // `email` must be unique
+  },
+  lastName: String,
+  password: String,
+  description: String,
+  profilePhoto: String,
+});
 
-module.export = model("User", usersSchema)
+module.export = model("User", usersSchema);
