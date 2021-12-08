c954b84dc5896cb8d966a6d709713bc662c851c6
“Daniel <danielvilches98@gmail.com>
2021-12-08 16:13:43 +0100

    auth&auth routes

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.status(200).json({ name: "Adrian" }));
 
+// Auth routes handling in app.js
+const authRouter = require("./routes/auth.routes");
+app.use("/auth", authRouter);
+
 module.exports = app;
 
 //test change
