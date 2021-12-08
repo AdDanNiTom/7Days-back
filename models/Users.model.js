d0c887ffa27f6ca77bb3fdd643b0f9740da0725b
AdrianAranda <adrienaranda@gmail.com>
2021-12-08 16:06:30 +0100

    Added Models

---
+++ models/Users.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const usersSchema = new Schema({
+    username: {
+        type: String,
+        unique: true // `username` must be unique
+      },
+    firstName: String,
+    email: {
+        type: String,
+        unique: true // `email` must be unique
+      },
+    lastName: String,
+    description: String,
+    profilePhoto: String,
+})
+
+module.export = model("User", usersSchema)
