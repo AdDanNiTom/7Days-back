f57c952790a96b381757bb1566bf025c4366ba51
“Daniel <danielvilches98@gmail.com>
2021-12-08 16:54:17 +0100

    auth&auth tested

--- models/Users.model.js
+++ models/Users.model.js
@@ -4,16 +4,22 @@ const { Schema, model } = mongoose;
 const usersSchema = new Schema({
     username: {
         type: String,
-        unique: true // `username` must be unique
+        unique: true, // `username` must be unique
+        required: true
       },
     firstName: String,
     email: {
         type: String,
-        unique: true // `email` must be unique
+        unique: true, // `email` must be unique
+        required: true
       },
     lastName: String,
+    password: {
+      type: String, 
+      required: true
+    },
     description: String,
     profilePhoto: String,
 })
 
-module.export = model("User", usersSchema)
+module.exports = model("User", usersSchema)
