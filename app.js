5d15c21f642d74b5b25c057004b609706f8b3140
AdrianAranda <adrienaranda@gmail.com>
2021-12-09 12:29:26 +0100

    added folder and started working on the file

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.get("/", (req, res) => res.status(200).json({ name: "Adrian" }));
 
 // Auth routes handling in app.js
 const authRouter = require("./routes/auth.routes");
+const usersRouter = require("./routes/users.routes");
+
 app.use("/auth", authRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
