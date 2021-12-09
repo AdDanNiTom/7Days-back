2e6d4afb82b0195a364d34c406fd17d7f32d8e4b
nicomellon <nmellon94@gmail.com>
2021-12-09 20:03:08 +0100

    users routes finished

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -20,63 +20,87 @@ router.get("/", async (req, res) => {
         )
       );
   } catch (err) {
-    console.log(err);
+    res
+      .status(500)
+      .json(createResponseObject(false, res.statusCode, err.message, null));
   }
 });
 
-// GET - FINDS ONE USER
-router.get("/:userId", async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const foundUser = await User.findById(userId);
+router
+  .route("/:userId")
+  // GET - FINDS ONE USER
+  .get(async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const foundUser = await User.findById(userId);
 
-    if (foundUser)
+      if (foundUser)
+        res
+          .status(200)
+          .json(
+            createResponseObject(
+              true,
+              res.statusCode,
+              `User with id: ${userId} found successfully.`,
+              foundUser
+            )
+          );
+      else {
+        res.status(404);
+        throw new Error("No user found with the specified id.");
+      }
+    } catch (err) {
+      res.json(createResponseObject(false, res.statusCode, err.message, null));
+    }
+  })
+  // PUT - EDIT USER INFO
+  .put(async (req, res) => {
+    try {
+      const { userId } = req.params;
+
+      if (!mongoose.Types.ObjectId.isValid(userId)) {
+        res.status(404);
+        throw new Error("Specified id is not valid");
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+      });
+
       res
         .status(200)
         .json(
           createResponseObject(
             true,
             res.statusCode,
-            `User with id: ${userId} found successfully.`,
-            foundUser
+            "User profile updated successfully.",
+            updatedUser
           )
         );
-    else {
-      res.status(404);
-      throw new Error("No user found with the specified id.");
+    } catch (err) {
+      res.json(createResponseObject(false, res.statusCode, err.message, null));
     }
-  } catch (err) {
-    res.json(createResponseObject(false, res.statusCode, err.message, null));
-  }
-});
-
-// PUT - EDITS USER INFO
-router.put("/:userId/edit", async (req, res) => {
-  try {
-    const { userId } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(userId)) {
-      res.status(404);
-      throw new Error("Specified id is not valid");
+  })
+  // DELETE - DELETE A USER
+  .delete(async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const deletedUser = await User.findByIdAndDelete(userId);
+      res
+        .status(200)
+        .json(
+          createResponseObject(
+            true,
+            res.statusCode,
+            "User account successfully deleted",
+            deletedUser
+          )
+        );
+    } catch (err) {
+      res
+        .status(500)
+        .json(createResponseObject(false, res.statusCode, err.message, null));
     }
-
-    const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
-      new: true,
-    });
-
-    res
-      .status(200)
-      .json(
-        createResponseObject(
-          true,
-          res.statusCode,
-          "User profile updated successfully.",
-          updatedUser
-        )
-      );
-  } catch (err) {
-    res.json(createResponseObject(false, res.statusCode, err.message, null));
-  }
-});
+  });
 
 module.exports = router;
