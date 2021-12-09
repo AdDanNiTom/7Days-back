b66a8b681506a5df9826553208d5e2e4a885a2e5
nicomellon <nmellon94@gmail.com>
2021-12-09 13:54:57 +0100

    fixes bug in event routes

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -4,33 +4,40 @@ const Event = require("../models/Events.model");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-import { useContext } from 'react';
-import {AuthContext} from "../context/auth.context"
-const {user} = useContext(AuthContext)
+router.get("/", (req, res) => {
+  const allEvents = Event.find();
+  res
+    .status(400)
+    .json({ message: "These are all of the events: " + allEvents });
+});
 
-router.get("/", (req, res)=>{
-    const allEvents = Event.find()
-    res.status(400).json({message:"These are all of the events: " + allEvents})
-})
+router.post("/new", (req, res) => {
+  const { title, description, location, icon, eventDate, maxAtendees } =
+    req.body;
 
-router.post("/new", (req, res)=>{
-    const {title, description, location, icon, eventDate, maxAtendees} = req.body
+  if (title === "") {
+    res.status(400).json({ message: "Please provide a title for the event." });
+    return;
+  }
 
-    if (title === "") {
-        res.status(400).json({ message: "Please provide a title for the event." });
-        return;
-    }
-
-    Event.create({title, description,owner:user._id, location, attendees: [], icon, eventDate, maxAtendees})
+  Event.create({
+    title,
+    description,
+    owner: user._id,
+    location,
+    attendees: [],
+    icon,
+    eventDate,
+    maxAtendees,
+  })
     .then((createdEvent) => {
-        // Send a json response containing the new event
-        res.status(201).json({createdEvent});
+      // Send a json response containing the new event
+      res.status(201).json({ createdEvent });
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({ message: "Internal Server Error" })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Internal Server Error" });
     });
-
-})
+});
 
 module.exports = router;
