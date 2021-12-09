daf9b6dce3a980be1523a9d902645e983395f1aa
AdrianAranda <adrienaranda@gmail.com>
2021-12-09 15:17:09 +0100

    Working version

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,37 +1,47 @@
 const express = require("express");
-const bcrypt = require('bcryptjs');
-const jwt = require("jsonwebtoken");
 const User = require("../models/Users.model");
 const mongoose = require("mongoose");
-const { isAuthenticated } = require('./../middleware/jwt.middleware.js');
-
 const router = express.Router();
-const saltRounds = 10;
 
-
-// POST /auth/signup  - Creates a new user in the database
-router.get('/', (req, res, next) => {
-    const allUsers = User.find()
+// GET - DISPLAYS ALL USERS
+router.get("/", async (req, res) => {
+  try {
+    const allUsers = await User.find();
+    console.log("hey");
+    res.json(allUsers);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-
-// POST  /auth/login - Verifies email and password and returns a JWT
-router.get('/:id', (req, res, next) => {
-    
+// GET - FINDS ONE USER
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const oneUser = await User.findById(id);
+    res.json(oneUser);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
+// PUT - EDITS USER INFO
+router.put("/:id/edit", async (req, res) => {
+  try {
+    const { id } = req.params;
 
-// GET  /auth/verify  -  Used to verify JWT stored on the client
-router.get('/verify', isAuthenticated, (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ message: "Specified id is not valid" });
+      return;
+    }
 
-  // If JWT token is valid the payload gets decoded by the
-  // isAuthenticated middleware and made available on `req.payload`
-  console.log(`req.payload`, req.payload);
-
-  // Send back the object with user data
-  // previously set as the token payload
-  res.status(200).json(req.payload);
+    const updatedProject = await Project.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedProject);
+  } catch (error) {
+    res.json(error);
+  }
 });
-
 
 module.exports = router;
