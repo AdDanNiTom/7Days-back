af87623c1387b4f2deacbab2287e7629205df510
nicomellon <nmellon94@gmail.com>
2021-12-10 12:09:37 +0100

    ready for testing

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -4,91 +4,147 @@ const Event = require("../models/Events.model");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-console.log("event.routes.js has been loaded")
+const createResponseObject = require("../utils/createResponseObject");
 
-// Delete event
-router.delete("/delete/:eventId", async (req, res) => {
-    try{
-        const eventId = req.params.eventId;
-        const deletedEvent = await Event.findByIdAndDelete(eventId)
-        res.json(deletedEvent)
-    } catch(err){
-        console.log(err)
-    }
-})
-
-// Edit event
-router.route("/edit/:eventId")
-.get(async (req, res) => {
-    try{
-        const eventId = req.params.eventId;
-        const foundEvent = await Event.findById(eventId)
-        res.status(201).json(foundEvent)
-
-    } catch(err) {
-        console.log(err)
-    }
-})
-.put(async (req, res) => {
-    try{
-        const { title, description, eventDate, maxAtendees } = req.body;
-        const updatedEvent = await Event.findByIdAndUpdate(req.params.eventId, {
-            title,
-            description,
-            eventDate,
-            maxAtendees
-        });
-        res.status(201).json({updatedEvent});
-    } catch(err) {
-        console.log(err)
-    }
-})
-
-// Show details of an event
-router.get("/:eventId", async (req, res) => {
-    try{
-        const eventId = req.params.eventId;
-        const eventDetails = await Event.findById(eventId)
-        res.json(eventDetails)
-    }
-    catch (err){
-        console.log(err)
-    }
-})
-
-// Create a new event
-router.post("/new", (req, res)=>{
-    const {title, description, icon, eventDate, maxAtendees} = req.body
-    
-    if (title === "") {
-        res.status(400).json({ message: "Please provide the required information for the event." });
-        return;
-    }
-    
-    Event.create({title, description, icon, attendees:[], eventDate, maxAtendees})
-    .then((createdEvent) => {
-        // Send a json response containing the new event
-        console.log("the event has been created")
-        res.status(201).json({createdEvent});
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({ message: "Internal Server Error" })
-    });
-    
-})
-
-// Get all events
-router.route("/")
-.get(async (req, res)=>{
+router
+  .route("/:eventId")
+  // GET - Gets event info
+  .get(async (req, res) => {
     try {
-        const allEvents = await Event.find()
-        console.log("allEvents: ", allEvents)
-        res.json(allEvents)
+      const eventId = req.params.eventId;
+      const eventDetails = await Event.findById(eventId);
+      res
+        .status(200)
+        .json(
+          createResponseObject(
+            true,
+            res.statusCode,
+            "Retrieved event information successfuly",
+            eventDetails
+          )
+        );
+    } catch (err) {
+      res
+        .status(500)
+        .json(createResponseObject(false, res.statusCode, err.message, null));
     }
-    catch (err) {
-        console.log(err)
-    }  
-})
+  })
+  // PUT - Edits an event
+  .put(async (req, res) => {
+    try {
+      const { title, description, eventDate, maxAtendees } = req.body;
+      const updatedEvent = await Event.findByIdAndUpdate(req.params.eventId, {
+        title,
+        description,
+        eventDate,
+        maxAtendees,
+      });
+      res
+        .status(200)
+        .json(
+          createResponseObject(
+            true,
+            res.statusCode,
+            "Event edited successfully",
+            updatedEvent
+          )
+        );
+    } catch (err) {
+      res
+        .status(500)
+        .json(createResponseObject(false, res.statusCode, err.message, null));
+    }
+  })
+  // DELETE - Deletes an event
+  .delete(async (req, res) => {
+    try {
+      const eventId = req.params.eventId;
+      const deletedEvent = await Event.findByIdAndDelete(eventId);
+      res
+        .status(200)
+        .json(
+          createResponseObject(
+            true,
+            res.statusCode,
+            "Event deleted successfully",
+            deletedEvent
+          )
+        );
+    } catch (err) {
+      res
+        .status(500)
+        .json(createResponseObject(false, res.statusCode, err.message, null));
+    }
+  });
+
+router
+  .route("/")
+  // GET - Get all events
+  .get(async (req, res) => {
+    try {
+      const allEvents = await Event.find();
+      res
+        .status(200)
+        .json(
+          createResponseObject(
+            true,
+            res.statusCode,
+            "List of events retrieved successfully",
+            allEvents
+          )
+        );
+    } catch (err) {
+      res
+        .status(500)
+        .json(createResponseObject(false, res.statusCode, err.message, null));
+    }
+  })
+  // POST - Create a new event
+  .post((req, res) => {
+    const { title, description, icon, eventDate, maxAtendees } = req.body;
+
+    if (!title) {
+      //   res
+      //     .status(400)
+      //     .json(
+      //       createResponseObject(
+      //         false,
+      //         res.statusCode,
+      //         "Please provide the required information for the event.",
+      //         null
+      //       )
+      //     );
+      //   return;
+      res.status(400);
+      throw new Error("Please provide the required information for the event.");
+    }
+
+    Event.create({
+      title,
+      description,
+      icon,
+      attendees: [],
+      eventDate,
+      maxAtendees,
+    })
+      .then((createdEvent) => {
+        // Send a json response containing the new event
+        res
+          .status(201)
+          .json(
+            createResponseObject(
+              true,
+              res.statusCode,
+              "the event has been created",
+              createdEvent
+            )
+          );
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json(createResponseObject(false, res.statusCode, err.message, null));
+      });
+  });
 
 module.exports = router;
